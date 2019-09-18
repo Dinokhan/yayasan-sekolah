9891cdabafd4d67331d79aaeaffcb9349b06a5e3
dinokhan <rizqidino08@gmail.com>
2019-09-18 20:13:37 +0700

    yayasan

---
+++ galeri.php
@@ -0,0 +1,314 @@
+<!DOCTYPE HTML>
+<!--
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>KOETWAKNO PRAMUHADININGRAT</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body class="homepage">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<div id="header-wrapper" class="wrapper">
+			<img style="float: right;" src="logo.png" width="20%" height="20%" >
+			<img style="float: left;" src="logo.png" width="20%" height="20%" >
+
+			<div id="header">
+
+				<!-- Logo -->
+				<div id="logo">
+
+					<h1><a href="index.php">KOETWAKNO <br><br>PRAMUHADININGRAT</a></h1><br>
+					<p>Yayasan Lembaga Pembelajaran</p>
+				</div>
+
+				<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="index.php">beranda</a></li>
+						<li>
+							<a href="#">program</a>
+							<ul>
+								<li><a href="#">Kel. orang tua asih</a></li>
+								<li><a href="#">pendidikan anak usia dini</a></li>
+								<li><a href="#">tk - al qur'an</a></li>
+								<li><a href="#">diniyah takmiliyah</a></li>
+								<!-- <li>
+									<a href="#">Sed consequat</a>
+									<ul>
+										<li><a href="#">Lorem dolor</a></li>
+										<li><a href="#">Amet consequat</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam nisl</a></li>
+										<li><a href="#">Sed feugiat</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Nisl tempus</a></li> -->
+							</ul>
+						</li>
+						<li>
+							<a href="#">Profile</a>
+							<ul>
+								<li><a href="galeri.php">Galeri Yayasan</a></li>
+								<li><a href="#">Visi dan Misi</a></li>
+								<li><a href="#">sejarah singkat</a></li>
+								<li><a href="#">sarana & prasarana</a></li>
+								<li><a href="#">stuktur organisasi</a></li>
+								<li><a href="#">admistrator</a></li>
+								
+							</ul>
+						</li>
+						<li><a href="left-sidebar.html">info pendaftaran</a></li>
+						<li><a href="left-sidebar.html">daftar</a></li><br>
+						<li><a href="left-sidebar.html">info yayasan</a></li>
+						<li><a href="right-sidebar.html">Guru</a></li>
+						<li><a href="no-sidebar.html">siswa</a></li>
+						<li><a href="tentang.php">tentang kami</a></li>
+						
+					</ul>
+				</nav>
+
+			</div>
+		</div>
+		<!-- Main -->
+		<div class="wrapper style2">
+			<div class="title">Galeri Kami</div>
+			<div id="main" class="container">
+
+				<!-- Content -->
+				<div id="content">
+					<article class="box post">
+						<header class="style1">
+							<h2>Ini adalah galeri dari yayasan kami <br class="mobile-hide" />
+							mulai dari guru hingga siswa </h2>
+							<p>Tempus feugiat veroeros sed nullam dolore</p>
+						</header>
+						<a href="foto/awal.jpg" class="image featured">
+							<img src="foto/awal.jpg" height="60%" ">
+						</a>
+						<p align="center">Akan kami tampilkan beberapa hasil dari perfotoan kami.</p>
+						
+					</article>
+					<div class="row 150%">
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Pencak Silat</h2>
+								</header>
+								<a href="foto/2.jpg" class="image featured"><img src="foto/2.jpg" alt="" /></a>
+								<p align="center">Dalam acara pelatihan pencak silat.</p>
+								
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Kerajinan Tangan</h2>
+								</header>
+								<a href="foto/3.jpg" class="image featured"><img src="foto/3.jpg" alt="" /></a>
+								<p align="center">Dalam rangka melatih kreatifitas murid dalam membuat suatu barang menjadi lebih berguna.</p>
+
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Perkemahan</h2>
+								</header>
+								<a href="foto/4.jpg" class="image featured"><img src="foto/4.jpg" alt="" /></a>
+								<p>Mengadakan acara perkemahan kepada anak didik, agar lebih memahami lebih tentang dunia alam.</p>
+							</section>
+						</div>
+					</div>
+					<!-- page ke dua -->
+					<div class="row 150%">
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Pencak Silat</h2>
+								</header>
+								<a href="foto/5.jpg" class="image featured"><img src="foto/5.jpg" alt="" /></a>
+								<p align="center">Dalam acara pelatihan pencak silat.</p>
+								
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Kerajinan Tangan</h2>
+								</header>
+								<a href="foto/6.jpg" class="image featured"><img src="foto/6.jpg" alt="" /></a>
+								<p align="center">Dalam rangka melatih kreatifitas murid dalam membuat suatu barang menjadi lebih berguna.</p>
+
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Perlombaan</h2>
+								</header>
+								<a href="foto/7.jpg" class="image featured"><img src="foto/7.jpg" alt="" /></a>
+								<p align="center">Acara lomba dapat mengasah ketangkasan terhadap anak didik dalam belajars.</p>
+							</section>
+						</div>
+					</div>
+					<!-- page ke 3 -->
+					<div class="row 150%">
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Kerajinan Tangan</h2>
+								</header>
+								<a href="foto/8.jpg" class="image featured"><img src="foto/8.jpg" alt="" /></a>
+								<p align="center">Dalam rangka melatih kreatifitas murid dalam membuat suatu barang menjadi lebih berguna.</p>
+								
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Kerajinan Tangan</h2>
+								</header>
+								<a href="foto/9.jpg" class="image featured"><img src="foto/9.jpg" alt="" /></a>
+								<p align="center">Dalam rangka melatih kreatifitas murid dalam membuat suatu barang menjadi lebih berguna.</p>
+
+							</section>
+						</div>
+						<div class="4u 12u(mobile)">
+							<section class="box">
+								<header>
+									<h2>Study Tour</h2>
+								</header>
+								<a href="foto/11.jpg" class="image featured"><img src="foto/11.jpg" alt="" /></a>
+								<p align="center">Untuk memperkenalkan kepada anggota mengenai study akademik</p>
+							</section>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		
+		<!-- Footer -->
+		<div id="footer-wrapper" class="wrapper">
+			<div class="title">tentang kami</div>
+			<div id="footer" class="container">
+				<header class="style1">
+					<h2>Apakah Yayasan Koetwakno Pramudiningrat?</h2>
+					<p>
+						Sebuah yayasan yang bergerak dalam sebuah otomasi pembelajaran dan multiguna.<br />
+						Dan dibangun bertujuan untuk membangun <br>sebuah karakter bangsa dan bernegara.
+					</p>
+				</header>
+				<hr />
+				<div class="row 150%">
+					<div class="6u 12u(mobile)">
+
+						<!-- Contact Form -->
+						<section>
+							<form method="post" action="#">
+								<div class="row 50%">
+									<div class="6u 12u(mobile)">
+										<input type="text" name="name" id="contact-name" placeholder="Nama" />
+									</div>
+									<div class="6u 12u(mobile)">
+										<input type="text" name="email" id="contact-email" placeholder="Email" />
+									</div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<textarea name="message" id="contact-message" placeholder="Tanyakan Apa saja ke kami" rows="4"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<ul class="actions">
+											<li><input type="submit" class="style1" value="Kirim" /></li>
+											<li><input type="reset" class="style2" value="Batal" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</section>
+
+					</div>
+					<div class="6u 12u(mobile)">
+
+						<!-- Contact -->
+						<section class="feature-list small">
+							<div class="row">
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-home">Alamat</h3>
+										<p >
+											Jl. Sumber Asih 32 - 25 RT/RW 01/09 <br />
+											Kel. Babakan Kec. Babakan Ciparay<br />
+											, Perumahan Sumber Sari Indah Bandung 40223
+										</p>
+									</section>
+								</div>
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-comment">Social</h3>
+										<p>
+											<a href="#">@stwdas</a><br />
+											<a href="#">stwdas@gmail</a><br />
+											<a href="#">facebook.com/stwdas</a>
+										</p>
+									</section>
+								</div>
+							</div>
+							<div class="row">
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-envelope">Email</h3>
+										<p>
+											<a href="#">stwdas@gmail.com</a>
+										</p>
+									</section>
+								</div>
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-phone">Telepon</h3>
+										<p>
+											0812-7222-5132
+										</p>
+									</section>
+								</div>
+							</div>
+						</section>
+
+					</div>
+				</div>
+				<hr />
+			</div>
+			<div id="copyright">
+				<ul>
+					<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Scripts -->
+
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/skel-viewport.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
---
+++ tentang.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<!--
+	Escape Velocity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>KOETWAKNO PRAMUHADININGRAT</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body class="homepage">
+	<div id="page-wrapper">
+
+		<!-- Header -->
+		<div id="header-wrapper" class="wrapper">
+			<img style="float: right;" src="logo.png" width="20%" height="20%" >
+			<img style="float: left;" src="logo.png" width="20%" height="20%" >
+
+			<div id="header">
+
+				<!-- Logo -->
+				<div id="logo">
+
+					<h1><a href="index.php">KOETWAKNO <br><br>PRAMUHADININGRAT</a></h1><br>
+					<p>Yayasan Lembaga Pembelajaran</p>
+				</div>
+
+				<!-- Nav -->
+				<nav id="nav">
+					<ul>
+						<li class="current"><a href="index.php">beranda</a></li>
+						<li>
+							<a href="#">program</a>
+							<ul>
+								<li><a href="#">Kel. orang tua asih</a></li>
+								<li><a href="#">pendidikan anak usia dini</a></li>
+								<li><a href="#">tk - al qur'an</a></li>
+								<li><a href="#">diniyah takmiliyah</a></li>
+								<!-- <li>
+									<a href="#">Sed consequat</a>
+									<ul>
+										<li><a href="#">Lorem dolor</a></li>
+										<li><a href="#">Amet consequat</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam nisl</a></li>
+										<li><a href="#">Sed feugiat</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Nisl tempus</a></li> -->
+							</ul>
+						</li>
+						<li>
+							<a href="#">Profile</a>
+							<ul>
+								<li><a href="#">Profil Yayasan</a></li>
+								<li><a href="#">Visi dan Misi</a></li>
+								<li><a href="#">sejarah singkat</a></li>
+								<li><a href="#">sarana & prasarana</a></li>
+								<li><a href="#">stuktur organisasi</a></li>
+								<li><a href="#">admistrator</a></li>
+								
+							</ul>
+						</li>
+						<li><a href="left-sidebar.html">info pendaftaran</a></li>
+						<li><a href="left-sidebar.html">daftar</a></li><br>
+						<li><a href="left-sidebar.html">info yayasan</a></li>
+						<li><a href="right-sidebar.html">Guru</a></li>
+						<li><a href="no-sidebar.html">siswa</a></li>
+						<li><a href="tentang.php">tentang kami</a></li>
+						
+					</ul>
+				</nav>
+
+			</div>
+		</div>
+	
+		<!-- Footer -->
+		<div id="footer-wrapper" class="wrapper">
+			<div class="title">tentang kami</div>
+			<div id="footer" class="container">
+				<header class="style1">
+					<h2>Apakah Yayasan Koetwakno Pramudiningrat?</h2>
+					<p>
+						Sebuah yayasan yang bergerak dalam sebuah otomasi pembelajaran dan multiguna.<br />
+						Dan dibangun bertujuan untuk membangun <br>sebuah karakter bangsa dan bernegara.
+					</p>
+				</header>
+				<hr />
+				<div class="row 150%">
+					<div class="6u 12u(mobile)">
+
+						<!-- Contact Form -->
+						<section>
+							<form method="post" action="#">
+								<div class="row 50%">
+									<div class="6u 12u(mobile)">
+										<input type="text" name="name" id="contact-name" placeholder="Nama" />
+									</div>
+									<div class="6u 12u(mobile)">
+										<input type="text" name="email" id="contact-email" placeholder="Email" />
+									</div>
+								</div>
+								<div class="row 50%">
+									<div class="12u">
+										<textarea name="message" id="contact-message" placeholder="Tanyakan Apa saja ke kami" rows="4"></textarea>
+									</div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<ul class="actions">
+											<li><input type="submit" class="style1" value="Kirim" /></li>
+											<li><input type="reset" class="style2" value="Batal" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</section>
+
+					</div>
+					<div class="6u 12u(mobile)">
+
+						<!-- Contact -->
+						<section class="feature-list small">
+							<div class="row">
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-home">Alamat</h3>
+										<p >
+											Jl. Sumber Asih 32 - 25 RT/RW 01/09 <br />
+											Kel. Babakan Kec. Babakan Ciparay<br />
+											, Perumahan Sumber Sari Indah Bandung 40223
+										</p>
+									</section>
+								</div>
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-comment">Social</h3>
+										<p>
+											<a href="#">@stwdas</a><br />
+											<a href="#">stwdas@gmail</a><br />
+											<a href="#">facebook.com/stwdas</a>
+										</p>
+									</section>
+								</div>
+							</div>
+							<div class="row">
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-envelope">Email</h3>
+										<p>
+											<a href="#">stwdas@gmail.com</a>
+										</p>
+									</section>
+								</div>
+								<div class="6u 12u(mobile)">
+									<section>
+										<h3 class="icon fa-phone">Telepon</h3>
+										<p>
+											0812-7222-5132
+										</p>
+									</section>
+								</div>
+							</div>
+						</section>
+
+					</div>
+				</div>
+				<hr />
+			</div>
+			<div id="copyright">
+				<ul>
+					<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Scripts -->
+
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/jquery.dropotron.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/skel-viewport.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
